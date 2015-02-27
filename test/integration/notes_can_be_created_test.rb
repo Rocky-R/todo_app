9a7d4a8c988e0b86ce015c681318e4c64f1742da
Rocky Randalls <rrandalls@gmail.com>
2015-02-27 11:44:08 -0500

    Add test for Note creation

---
+++ test/integration/notes_can_be_created_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class NotesCanBeCreatedTest < ActionDispatch::IntegrationTest
+  test "notes can be created" do
+    get root_path
+    get new_note_path
+    assert_select("form")
+    assert_difference('Note.count', 1) do
+      Note.create(title: 'Do lots of stuff',
+          description: 'and much stuff there was to be done')
+    end
+  end
+end
